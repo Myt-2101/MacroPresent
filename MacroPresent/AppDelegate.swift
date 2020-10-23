6c1d5eebe78ada7fb682f9c03ebf4e16d2d45c01
Myt-2101 <73332461+Myt-2101@users.noreply.github.com>
2020-10-23 15:20:09 +0700

    Initial Commit

---
+++ MacroPresent/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacroPresent
+//
+//  Created by anandito baramayasi on 23/10/20.
+//  Copyright © 2020 CKCK. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacroPresent/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacroPresent
+//
+//  Created by anandito baramayasi on 23/10/20.
+//  Copyright © 2020 CKCK. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
