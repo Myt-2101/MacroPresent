9c27fc71ce098a79b0cf10f3ed407c26d400e7cc
aditya26-dev <dewaa.kadek26@gmail.com>
2020-11-03 16:05:50 +0800

    Sudah SetTimer

--- MacroPresent/KeynoteTimer.swift
+++ MacroPresent/KeynoteTimer.swift
@@ -15,6 +15,15 @@ class KeynoteTimer: NSViewController {
         // Do view setup here.
     }
     
+    var timer : Timer?
+    var timerPerSlide : Timer?
+    var timeInSeconds = 0
+    var timerInSeconds = 0
+    var averageTimePerSlide : Int = 0
+    var arrayTimePerSlide = [Int]()
+    var formatter = DateComponentsFormatter()
+    var interval : TimeInterval = 0
+    
     @IBOutlet weak var keynoteName: NSTextField!
     
     @IBOutlet weak var totalTime: NSTextField!
@@ -22,6 +31,34 @@ class KeynoteTimer: NSViewController {
     @IBOutlet weak var timePerSlide: NSTextField!
     
     @IBAction func nextButton(_ sender: Any) {
+    }
+    
+    func startTotalTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(startInSeconds), userInfo: nil, repeats: true)
+    }
+    
+    @objc func startInSeconds(){
+        timeInSeconds += 1
+    }
+    
+    func startTimePerSlide(){
+        timerPerSlide = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(startPerSlide), userInfo: nil, repeats: true)
+    }
+    
+    @objc func startPerSlide(){
+        timerInSeconds += 1
+    }
+    
+    func newSlideTimer(){
+        arrayTimePerSlide.append(timerInSeconds)
+        timerInSeconds = 0
+        averageTimePerSlide = timeInSeconds / arrayTimePerSlide.count
+        interval = TimeInterval(averageTimePerSlide)
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .abbreviated
+        
+        //Diassign ke label time per slide
+        formatter.string(from: interval)
     }
     
 }
