c927b7db39257cae0c94662fb65308a1823b6e45
aditya26-dev <dewaa.kadek26@gmail.com>
2020-11-06 18:10:24 +0800

    "ChooseFile Done"

--- MacroPresent/HomeViewController.swift
+++ MacroPresent/HomeViewController.swift
@@ -8,12 +8,21 @@
 
 import Cocoa
 
+let getFileName =
+"""
+    tell application "Finder"
+        open POSIX file "%@"
+    end tell
+"""
+
 class HomeViewController: NSViewController {
 
     @IBOutlet weak var helloText: NSTextField!
     @IBOutlet weak var keynoteTextField: NSTextField!
     @IBOutlet weak var maxDurationTextField: NSTextField!
     @IBOutlet weak var historyPlaceHolderText: NSTextField!
+    
+    var keynotFilePath = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +31,34 @@ class HomeViewController: NSViewController {
     
     
     @IBAction func onChooseFile(_ sender: NSButton) {
+        let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = false
+        openPanel.canCreateDirectories = false
+        openPanel.canChooseFiles = true
+        openPanel.allowedFileTypes = ["key"]
+        openPanel.begin { (result) -> Void in
+            if result.rawValue == NSApplication.ModalResponse.OK.rawValue {
+                //Do what you will
+                //If there's only one URL, surely 'openPanel.URL'
+                //but otherwise a for loop works
+                if let resultString = openPanel.url?.path {
+                    self.keynoteTextField.stringValue = resultString
+                    self.keynotFilePath = String(format: getFileName, resultString)
+                }
+                
+            }
+        }
     }
     
     @IBAction func onStartPractice(_ sender: NSButton) {
+        let script = NSAppleScript(source: keynotFilePath)
+        var error = NSDictionary?.none
+        script?.executeAndReturnError(&error)
+        print(error)
     }
     
     @IBAction func onStepperChange(_ sender: NSStepper) {
     }
+
 }
